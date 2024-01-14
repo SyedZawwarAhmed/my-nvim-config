cebbca96e80be3fadfb4891c80cf21d0922cfa91
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-01-14 19:20:38 +0500

    format telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,11 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fw', function () 
+vim.keymap.set('n', '<leader>fw', function()
     builtin.live_grep {
         grep_open_files = true,
-        grep_flags = {"-i", "-F"}
+        grep_flags = { "-i", "-F" }
     }
 end, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
