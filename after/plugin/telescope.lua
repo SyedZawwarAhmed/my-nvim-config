db02cd4bc0533bd1481d58abe8495e02103b0328
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-01-18 09:53:15 +0500

    modify live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,30 @@
 local builtin = require('telescope.builtin')
+
+local actions = require('telescope.actions')
+require('telescope').setup{
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      -- '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      -- '--smart-case',
+      '-F'
+    },
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+    },
+  }
+}
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fw', function()
-    builtin.live_grep {
-        grep_open_files = true,
-        grep_flags = { "-i", "-F" }
-    }
-end, {})
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fs', [[<Cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})<CR>]], { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { noremap = true, silent = true })
