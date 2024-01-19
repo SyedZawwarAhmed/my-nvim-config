a630cd4b93aa2197d2c05e664989e40738076527
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-01-19 10:49:49 +0500

    add more keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,18 +1,23 @@
 local lsp_zero = require('lsp-zero')
+local builtin = require('telescope.builtin')
 
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gr", builtin.lsp_references, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", 'gI', builtin.lsp_implementations, opts)
+  vim.keymap.set("n", '<leader>D', builtin.lsp_type_definitions, opts)
+  vim.keymap.set("n", '<leader>ds', builtin.lsp_document_symbols, opts)
+  vim.keymap.set("n", '<leader>ws', builtin.lsp_dynamic_workspace_symbols, opts)
 end)
 
 require('mason').setup({})
