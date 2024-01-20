0d49e4a5531a8fced11a292721161a0b4cdc6eec
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-01-21 00:17:23 +0500

    change formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -45,8 +45,8 @@ cmp.setup({
   },
   formatting = lsp_zero.cmp_format(),
   mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
