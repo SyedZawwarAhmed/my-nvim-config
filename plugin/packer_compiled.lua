51f147ec4a64270e4ea7efcc7272737a07d7d6a2
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-01-22 13:28:26 +0500

    change color column width

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -170,6 +170,11 @@ _G.packer_plugins = {
     path = "C:\\Users\\syawa\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-ts-context-commentstring"] = {
+    loaded = true,
+    path = "C:\\Users\\syawa\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-ts-context-commentstring",
+    url = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring"
+  },
   ["nvim-web-devicons"] = {
     loaded = true,
     path = "C:\\Users\\syawa\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-web-devicons",
