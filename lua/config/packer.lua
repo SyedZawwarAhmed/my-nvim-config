1783866c0645baeb64b290489e31f8737582fb39
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-01-30 12:21:52 +0500

    add vim-surround plugin

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -4,70 +4,72 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('tpope/vim-fugitive')
+  use('lewis6991/gitsigns.nvim')
+  use('mbbill/undotree')
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment the two plugins below if you want to manage the language servers from neovim
+      --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lua' },
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' }
     }
+  }
 
-    use { "catppuccin/nvim", as = "catppuccin" }
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('nvim-treesitter/playground')
-    use('theprimeagen/harpoon')
-    use('tpope/vim-fugitive')
-    use ('lewis6991/gitsigns.nvim')
-    use ('mbbill/undotree')
+  use {
+    'akinsho/flutter-tools.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+  }
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            --- Uncomment the two plugins below if you want to manage the language servers from neovim
-            --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
 
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lua' },
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' }
-        }
-    }
+  use('vim-airline/vim-airline')
 
-    use {
-        'akinsho/flutter-tools.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'stevearc/dressing.nvim', -- optional for vim.ui.select
-        },
-    }
+  use { 'neoclide/coc.nvim', branch = 'release' }
 
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    }
+  use { "gaelph/logsitter.nvim", requires = { "nvim-treesitter/nvim-treesitter" } }
 
-    use('vim-airline/vim-airline')
+  use("tpope/vim-commentary")
 
-    use { 'neoclide/coc.nvim', branch = 'release' }
+  use('JoosepAlviste/nvim-ts-context-commentstring')
 
-    use { "gaelph/logsitter.nvim", requires = { "nvim-treesitter/nvim-treesitter" } }
+  use("nvim-tree/nvim-tree.lua")
 
-    use("tpope/vim-commentary")
+  use("nvim-tree/nvim-web-devicons")
 
-    use('JoosepAlviste/nvim-ts-context-commentstring')
-
-    use("nvim-tree/nvim-tree.lua")
-
-    use("nvim-tree/nvim-web-devicons")
+  use("tpope/vim-surround")
 end)
