4598461819ddd2a59bc6c701cae058c9593d98d7
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-01-30 15:29:52 +0500

    update mapping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -47,7 +47,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
     }),
 })
