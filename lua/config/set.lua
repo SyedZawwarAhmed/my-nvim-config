aa31ec3ff5f49493de15fc3b05618977a7fc4431
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-01-30 12:20:18 +0500

    update set.lua

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -3,9 +3,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -28,7 +28,10 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "180"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.cmd [[
   augroup YankHighlight
