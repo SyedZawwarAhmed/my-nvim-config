816e9c73dba6559e20ee34f2eeca213a630601c9
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-02-04 08:54:53 +0500

    update remap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -30,8 +30,8 @@ local function telescope_live_grep_open_files()
 end
 
 vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
