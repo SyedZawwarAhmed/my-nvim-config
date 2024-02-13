b5064e9c678eda10b83290093d8dc18eba968808
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-02-13 08:31:38 +0500

    add emmet plugin

---
+++ after/plugin/emmet.lua
@@ -0,0 +1,2 @@
+vim.g.user_emmet_leader_key = '<c-m>'
+vim.g.user_emmet_settings = { indent_blockelement = 1 }
--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -72,4 +72,29 @@ return require('packer').startup(function(use)
   use("nvim-tree/nvim-web-devicons")
 
   use("tpope/vim-surround")
+
+  use {
+    'mattn/emmet-vim',
+    ft = { 'svelte', 'html', 'elixir', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+    config = function()
+      vim.g.user_emmet_settings = {
+        ['javascript.jsx'] = {
+          extends = 'jsx',
+        },
+        ['javascript.javascriptreact'] = {
+          extends = 'jsx',
+        },
+        ['typescript.typescriptreact'] = {
+          extends = 'jsx',
+        },
+        elixir = {
+          extends = 'html'
+        },
+        eelixir = {
+          extends = 'html'
+        }
+      }
+      vim.g.user_emmet_mode = 'inv'
+    end
+  }
 end)
