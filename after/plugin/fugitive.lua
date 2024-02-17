0c0654360eccca26b2733f050bcf8e36aea73f9b
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-02-17 14:59:04 +0500

    feat: add merge conflict keymaps in fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.api.nvim_set_keymap('n', '<leader>gs', ':vertical G<CR>', {noremap = true, silent = true})
+vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "gab", "<cmd>/^<<<<<<<\\s*/d | :/^=======\\s*/d | :/^>>>>>>>\\s*/d<CR>")
